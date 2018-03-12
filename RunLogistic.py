77d78e55a2cffa2d12eeb609c964443d71a08fde
ChaudhariHarsh <harshchaudhari@rocketmail.com>
2018-03-12 22:03:22 +0530

    Update RunLogistic.py
    commenting RunLogistic

--- RunLogistic.py
+++ RunLogistic.py
@@ -7,9 +7,9 @@ from supervisedlearning import *
 
 def logisticCall():
     Z = []
-    file = open("ex2data1.txt", "r") 
+    file = open("ex2data1.txt", "r")                            ## Read file with handle as file
     for line in file:
-        line = line.split(',')
+        line = line.split(',')                                  ## split line 
         Z.append(line)
     
     Z = np.matrix(Z, dtype=float)
@@ -18,10 +18,10 @@ def logisticCall():
     Y = np.zeros((j,1),dtype=float)
     Y_prediction = np.zeros((1,j),dtype=float)
     for i in range(j):
-        X[i,0]= Z[i,0]
-        X[i,1]= Z[i,0]**(1/2.0)
-        X[i,2]= Z[i,1]
-        Y[i,0]=Z[i,2]
+        X[i,0]= Z[i,0]                                          ## Feature-1
+        X[i,1]= Z[i,0]**(1/2.0)                                 ## Feature-2 as root
+        X[i,2]= Z[i,1]                                          ## Feature-3
+        Y[i,0]=Z[i,2]                                           ## Responses
     X = np.matrix(X)
     X = X.T ## Make this X with shape of (4,47)
     Y = Y.T ## Make this Y with shape of (1,47)
@@ -29,7 +29,7 @@ def logisticCall():
     hyponthsis_function, error, W, b = logistic_regression(X, Y, itertions=10000, learning_rate=0.01, method="Logistic")
     for i in range(hyponthsis_function.shape[1]):
         if (hyponthsis_function[0,i] > 0.5):
-            Y_prediction[0,i] = 1
+            Y_prediction[0,i] = 1                               ## classify 
         else:
             Y_prediction[0,i] = 0
         if Y_prediction[0,i]!=Y[0,i]:
@@ -43,7 +43,7 @@ def logisticCall():
     x2 = np.array(X[2,:]).flatten()
     y = np.array(Y[0,:]).flatten()
     y2 = 1-y
-    hf1 = np.array(hyponthsis_function[0,:]).flatten() - 0.5
+    hf1 = np.array(hyponthsis_function[0,:]).flatten() - 0.5    
     hf2 = 0.5 - np.array(hyponthsis_function[0,:]).flatten()
     ax.scatter(x1, x2,y*50,marker='+',c="b")
     ax.scatter(x1, x2,y2*50,marker='o',c="g")
