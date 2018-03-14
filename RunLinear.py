becb5e458a3803e4682e4d3486c744694cfd9272
ChaudhariHarsh <harshchaudhari@rocketmail.com>
2018-03-14 20:26:23 +0530

    Update RunLinear.py
    minor commenting

--- RunLinear.py
+++ RunLinear.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-from supervisedlearning import *
+from supervisedlearning import *       ## This is our supervised learning function file
 
 
 def linearCall():
